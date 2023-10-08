4ec78e6b1b6d241e9c28232903cd1b556723a16f
Sora <34794115+sorae42@users.noreply.github.com>
2023-10-09 00:18:57 +0700

    xong setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<p>Trang chủ </p>
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+    <Auth
+        supabaseClient={data.supabase}
+        view="sign_in"
+        redirectTo={`${data.url}/auth/callback`}
+        showLinks={false}
+        appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+    />
+</div>
