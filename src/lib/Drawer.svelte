95480ad688f8356931d45d4e2a7422db8cbed953
Sora <34794115+sorae42@users.noreply.github.com>
2023-10-17 00:29:37 +0700

    feat: ngăn kéo cho link

---
+++ src/lib/Drawer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	function close(): void {
+		drawerStore.close();
+	}
+</script>
+
+<!-- TODO: chỉnh link vào các trang cần thiết -->
+<nav class="list-nav p-4 [&>*>*>*]:!no-underline">
+	<ul>
+		<li><a href="/" on:click={close}>Homepage</a></li>
+		<li><a href="/about" on:click={close}>About</a></li>
+		<li><a href="/blog" on:click={close}>Blog</a></li>
+		<li><a href="/contact" on:click={close}>Contact</a></li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import '../app.postcss';
+	import DrawerComponent from '$lib/Drawer.svelte';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { initializeStores, storePopup, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import BurgerBtn from '$lib/BurgerBtn.svelte';
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
 
 	export let data;
 
@@ -22,14 +28,30 @@
 
 		return () => data.subscription.unsubscribe();
 	});
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
-<AppShell scrollbarGutter="auto">
+<Drawer>
+	<h2 class="p-4">Navigation</h2>
+	<hr />
+	<DrawerComponent />
+</Drawer>
+
+<AppShell scrollbarGutter="auto" slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+	<svelte:fragment slot="sidebarLeft">
+		<DrawerComponent />
+	</svelte:fragment>
 	
 	<svelte:fragment slot="header">
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Tra cứu kết quả học tập</strong>
+				<div>
+					<BurgerBtn onclickEvent={drawerOpen} />
+					<strong class="text-xl uppercase">Tra cứu kết quả học tập</strong>
+				</div>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
