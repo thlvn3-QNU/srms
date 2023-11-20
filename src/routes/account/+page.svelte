726389ed84fa8b6de003c7f0d6c8f7f18a7c2155
Sora <34794115+sorae42@users.noreply.github.com>
2023-11-20 16:15:39 +0700

    feat: account overview page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,45 +1,22 @@
 <script>
+	export let data;
+
+	let { session, supabase, profile } = data;
+	$: ({ session, supabase, profile } = data);
 </script>
 
-<div class="">
-	<div class="flex">
-		<div class="" />
-		<div class="flex-1">
-			<div class="  m-5 py-3 pl-3 text-lg font-medium">Thông tin ADMIN</div>
-			<div class="m-5">
-				<table class="grid grid-flow-col grid-col-3">
-					<thead class=" ">
-						<tr class="flex flex-col text-left w-full">
-							<th class=" pl-5">Họ tên </th>
-							<th class=" pl-5">Ngày sinh</th>
-							<th class=" pl-5">Nơi sinh</th>
-							<th class=" pl-5">Giới tính</th>
-							<th class=" pl-5">Số điện thoại</th>
-							<th class=" pl-5">Email</th>
-							<th class=" pl-5">Ngày tạo tài khoản</th>
-						</tr>
-					</thead>
-					<tbody class="col-span-2">
-						<tr class="flex flex-col">
-							<td>Nguyễn Văn A</td>
-							<td>1-01-1995</td>
-							<td>An Nhơn - Bình Định</td>
-							<td>Nam</td>
-							<td>039543534</td>
-							<td>Admin@gmail.com</td>
-							<td>1-01/2017</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-
-			<div class="text-right mr-[20px] my-[30px]">
-				<button
-					class="px-[10px] py-[5px] bg-blue-400 hover:bg-blue-300 text-[16px] font-medium rounded-[5px]"
-				>
-					Sửa thông tin
-				</button>
-			</div>
+<div class="flex justify-center">
+	<div class="card p-8 w-3/4 [&>*]:my-4">
+		<h2 class="h2">Tiểu sử</h2>
+		<!-- TODO: turn this into a table -->
+		<div>
+			<p>Họ tên: {profile?.full_name}</p>
+			<p>Ngày sinh: {profile?.date_of_birth}</p>
+			<p>Nơi sinh: Chưa cập nhật</p>
+			<p>Giới tính: {profile?.gender ? 'Nam' : 'Nữ'}</p>
+			<p>Số điện thoại: {'0' + profile?.phone_number || 'Chưa cập nhật'}</p>
+			<p>Email: {session.user.email}</p>
 		</div>
+		<button class="variant-filled">Sửa thông tin</button>
 	</div>
 </div>
