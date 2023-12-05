c87a5e001f2999316f6a7d72b9832541ee6aa273
Sora <34794115+sorae42@users.noreply.github.com>
2023-12-05 09:50:00 +0700

    chore: attempt to fix slow startup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	optimizeDeps: {
+		include: [
+			'@supabase/auth-helpers-sveltekit',
+			'@supabase/auth-ui-shared',
+			'@supabase/auth-ui-svelte',
+			'@skeletonlabs/skeleton',
+			'svelte-awesome-icons',
+			'@floating-ui/dom'
+		]
+	}
 });
