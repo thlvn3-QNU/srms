39473f1bd1f9b30bac8232a493344d9b1cf01c61
Sora <34794115+sorae42@users.noreply.github.com>
2023-12-09 09:20:35 +0700

    feat(subject): dialog to edit data

--- src/routes/manage/subject/DataModify.svelte
+++ src/routes/manage/subject/DataModify.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
 	import { getModalStore } from "@skeletonlabs/skeleton";
+	import type { SupabaseClient } from "@supabase/supabase-js";
 	import type { SvelteComponent } from "svelte";
 
 	export let parent: SvelteComponent;
 
 	const modalStore = getModalStore();
 
-    
+    const subjectId = $modalStore[0].meta.id
+    const submitAction = subjectId > 0 ? '?/update-subject' : '?/create-subject';
+    const subjectData = $modalStore[0].meta.data;
+
+    const supabase: SupabaseClient = $modalStore[0].meta.supabase;
 </script>
 
 {#if $modalStore[0]}
     <div class="card-modal">
-        <p>Thay đổi thông tin môn học</p>
-        <p>ID: {$modalStore[0].meta.id}</p>
+        <h3 class="h3">{subjectId > 0 ? 'Thay đổi thông tin' : 'Tạo mới'} môn học</h3>
+        {#if subjectId !== -1}
+            <p>ID: {subjectId}</p>
+        {/if}
+        <form action="{submitAction}" class="[&>*]:py-2">
+            <div>
+                <label for="subject">Tên môn học</label>
+                <input type="text" id="subject" name="subject" value="{subjectData?.name}">
+            </div>
+            <div>
+                <label for="credits">Tín chỉ</label>
+                <input type="text" id="credits" name="credits" value="{subjectData?.credits}">
+            </div>
+            <div>
+                <input type="submit" value="Lưu thông tin" class="w-min">
+                <button class="variant-filled-error" on:click={modalStore.close}>Huỷ bỏ</button>
+            </div>
+        </form>
     </div>
 {/if}
