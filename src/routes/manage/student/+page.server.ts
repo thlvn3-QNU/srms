e52461caa2e02f2443d2e911c73c64dc7bc9a11e
TPALT <tphualt@gmail.com>
2023-12-11 21:25:35 +0700

    Implemented update student profile feature
    This shows a modal form in-page when clicking 'Sửa" button, which lets you to edit profiles.

--- src/routes/manage/student/+page.server.ts
+++ src/routes/manage/student/+page.server.ts
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+import { randomUUID, type UUID } from 'crypto';
 
 export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
@@ -9,9 +10,47 @@ export const load: PageServerLoad = async ({ locals: { supabase, getSession } })
 	}
 
 	const { data: students } = await supabase
-		.from('profiles')
-		.select(`id, full_name, student_id, school_year`)
-        .eq('permission', 0);
+	.from('profiles')
+	.select()
+	.eq('permission', 0);
 
 	return { session, students };
 };
+
+export const actions = {
+    update: async ({ locals: { supabase }, request }) => {
+		const formData = await request.formData();
+		
+		let ID: any;
+		if ( formData.get('id') === 'undefined') {
+			ID = randomUUID();
+		}
+		else {
+			ID = formData.get('id');
+		}
+
+		let profile = {
+			id: ID,
+			student_id: formData.get('student_id'),
+			full_name: formData.get('full_name'),
+			date_of_birth: formData.get('date_of_birth'),
+			gender: formData.get('gender'),
+			address: formData.get('address'),
+			phone_number: formData.get('phone_number'),
+			class_name: formData.get('class_name'),
+			school_year: formData.get('school_year'),
+			permission: 0
+		}
+
+		// const { data, error } = await supabase.auth.admin.createUser({
+		// 	email: 'user@email.com',
+		// 	password: 'password',
+		// 	user_metadata: { name: 'Yoda' }
+		// })
+
+
+		await supabase
+		.from('profiles')
+		.upsert(profile);
+    },
+} satisfies Actions;
--- src/routes/manage/student/+page.svelte
+++ src/routes/manage/student/+page.svelte
@@ -1,22 +1,59 @@
 <script lang="ts">
-	import { Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
+	import { getModalStore, Table, tableMapperValues, type ModalSettings, type TableSource } from '@skeletonlabs/skeleton';
 
 	export let data;
+	const ADD_STUDENT_MODAL: number  = 1,
+	 	  EDIT_STUDENT_MODAL: number = 2,
+		  DELETE_STUDENT_MODAL: number = 3;
 
 	let { supabase, session, students } = data;
 	$: ({ supabase, session, students } = data);
 
 	let studentTable: any[] = students as any[];
 
-	const displayTable: TableSource = {
-		head: ['MSSV', 'Họ tên', 'Khoá'],
-		body: tableMapperValues(studentTable, ['student_id', 'full_name', 'school_year']),
-		meta: tableMapperValues(studentTable, ['id', 'student_id', 'full_name'])
-	};
-
 	function entrySelect(meta: any) {
 		console.log(meta);
+	};
+
+	const modalStore = getModalStore();
+
+	let fieldNames: any = [];
+	fieldNames = ['STT', 'MSV', 'Họ tên', 'Ngày sinh', 'Giới tính', 'Địa chỉ', 'Số điện thoại', 'Khoá', 'Lớp'];
+
+	function openModal(type: number, index: number) {
+		let UpcomingMeta: any;
+
+		if (type === ADD_STUDENT_MODAL) {
+			UpcomingMeta = {
+				type: ADD_STUDENT_MODAL,
+			}
+		}
+		else if (type === EDIT_STUDENT_MODAL){
+			UpcomingMeta = {
+				type: EDIT_STUDENT_MODAL,
+				data: {
+					id:            studentTable[index].id,
+					student_id:    studentTable[index].student_id,
+					full_name:     studentTable[index].full_name,
+					date_of_birth: studentTable[index].date_of_birth,
+					gender: 	   studentTable[index].gender,
+					address:       studentTable[index].address,
+					phone_number:  studentTable[index].phone_number,
+					school_year:   studentTable[index].school_year,
+					class_name:    studentTable[index].class_name,
+				},
+			};
+		};
+
+		let DataModal: ModalSettings = {
+			type: 'component',
+			component: 'modalComponent',
+			meta: UpcomingMeta,
+		};
+
+		modalStore.trigger(DataModal);
 	}
+
 </script>
 
 <div class="[&>*]:py-4">
@@ -28,7 +65,42 @@
 			<input type="text" name="search-box" id="search-box" placeholder="Tìm kiếm..." disabled />
 		</span>
 	</div>
-	<div class="content">
-		<Table source={displayTable} interactive={true} on:selected={entrySelect} />
+	
+	<div>
+		<button type="button" class="hover:variant-filled-error bg-green-500"
+				on:click={() => openModal(1, -1)}>Thêm</button> 
+	</div>
+	<div class="table-container">
+		<table class="table table-hover">
+			<thead class="text-center">
+				<tr>
+					{#each fieldNames as fName}
+					<th>{fName}</th>
+					{/each}
+				</tr>
+			</thead>
+			<tbody>
+				{#each studentTable as studentRow, i}
+					<tr>
+						<td>{i + 1}</td>
+						
+						<!-- Don't blame me -->
+						<td>{studentRow?.student_id}</td>
+						<td>{studentRow?.full_name}</td>
+						<td>{studentRow?.date_of_birth}</td>
+						<td>{studentRow?.gender ? 'Nữ' : 'Nam'}</td>
+						<td>{studentRow?.address}</td>
+						<td>{studentRow?.phone_number}</td>
+						<td>{studentRow?.school_year}</td>
+						<td>{studentRow?.class_name}</td>
+
+						<td>
+							<button type="button" class="hover:variant-filled-error bg-green-500"
+							 		on:click={() => openModal(EDIT_STUDENT_MODAL, i)}>Sửa</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
