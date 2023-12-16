b328db3aa2cbabea365b9097582dcf6b302dcf28
TPALT <tphualt@gmail.com>
2023-12-16 20:21:38 +0700

    Removes some unnecessary imports

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,5 @@
-import { invalidate } from '$app/navigation';
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL, PUBLIC_SECRET_ROLE_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL} from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
-import { createClient } from '@supabase/supabase-js'
 
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
