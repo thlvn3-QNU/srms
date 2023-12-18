43ea692b88e0249c3d49d000e5a4ca81ea00d46a
Sora <34794115+sorae42@users.noreply.github.com>
2023-12-18 08:03:03 +0700

    styling(student): move button as there's only one button

--- src/routes/manage/student/+page.svelte
+++ src/routes/manage/student/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+	import { PlusSolid } from 'svelte-awesome-icons';
 
 	export let data;
-	const ADD_STUDENT_MODAL: number  = 1,
-	 	  EDIT_STUDENT_MODAL: number = 2,
-		  DELETE_STUDENT_MODAL: number = 3;
+	const ADD_STUDENT_MODAL: number = 1,
+		EDIT_STUDENT_MODAL: number = 2,
+		DELETE_STUDENT_MODAL: number = 3;
 
 	let { supabase, session, students } = data;
 	$: ({ supabase, session, students } = data);
@@ -15,49 +16,58 @@
 	const modalStore = getModalStore();
 
 	let fieldNames: any = [];
-	fieldNames = ['STT', 'MSV', 'Họ tên', 'Ngày sinh', 'Giới tính', 'Địa chỉ', 'Số điện thoại', 'Khoá', 'Lớp'];
+	fieldNames = [
+		'STT',
+		'MSV',
+		'Họ tên',
+		'Ngày sinh',
+		'Giới tính',
+		'Địa chỉ',
+		'Số điện thoại',
+		'Khoá',
+		'Lớp',
+		''
+	];
 
 	function openModal(type: number, index: number) {
 		let UpcomingMeta: any;
 
 		if (type === ADD_STUDENT_MODAL) {
 			UpcomingMeta = {
-				type: ADD_STUDENT_MODAL,
-			}
-		}
-		else if (type === EDIT_STUDENT_MODAL){
+				type: ADD_STUDENT_MODAL
+			};
+		} else if (type === EDIT_STUDENT_MODAL) {
 			UpcomingMeta = {
 				type: EDIT_STUDENT_MODAL,
 				data: {
-					id:            studentTable[index].id,
-					student_id:    studentTable[index].student_id,
-					full_name:     studentTable[index].full_name,
+					id: studentTable[index].id,
+					student_id: studentTable[index].student_id,
+					full_name: studentTable[index].full_name,
 					date_of_birth: studentTable[index].date_of_birth,
-					gender: 	   studentTable[index].gender,
-					address:       studentTable[index].address,
-					phone_number:  studentTable[index].phone_number,
-					school_year:   studentTable[index].school_year,
-					class_name:    studentTable[index].class_name,
-				},
+					gender: studentTable[index].gender,
+					address: studentTable[index].address,
+					phone_number: studentTable[index].phone_number,
+					school_year: studentTable[index].school_year,
+					class_name: studentTable[index].class_name
+				}
 			};
-		}
-		else {
+		} else {
 			UpcomingMeta = {
 				type: DELETE_STUDENT_MODAL,
 				data: { id: studentTable[index].id }
-			}
+			};
 		}
 
 		let DataModal: ModalSettings = {
 			type: 'component',
 			component: 'modalComponent',
-			meta: UpcomingMeta,
+			meta: UpcomingMeta
 		};
 
 		modalStore.trigger(DataModal);
 	}
 
-	let SearchQuery: string = '';	
+	let SearchQuery: string = '';
 
 	// A pure Javascript function. The dev is dumb. Deal with it lol
 	function searchTable() {
@@ -70,11 +80,10 @@
 
 			for (let j = 0; j < CellList.length; j++) {
 				if (CellList[j] && CellList[j].innerHTML.toUpperCase().indexOf(query) > -1) {
-					RowList[i].style.display = "";
+					RowList[i].style.display = '';
 					break;
-				}
-				else {
-					RowList[i].style.display = "none";
+				} else {
+					RowList[i].style.display = 'none';
 				}
 			}
 		}
@@ -86,22 +95,29 @@
 		<span>
 			<h2 class="h2">Danh sách sinh viên</h2>
 		</span>
-		<span>
-			<input type="text" name="search-box" id="search-box" placeholder="Tìm kiếm..." bind:value={SearchQuery}
-				on:input={searchTable} />
+		<span class="flex gap-3">
+			<button
+				type="button"
+				class="variant-filled"
+				on:click={() => openModal(1, -1)}><PlusSolid size="16" /> Thêm</button
+			>
+			<input
+				type="text"
+				name="search-box"
+				id="search-box"
+				placeholder="Tìm kiếm..."
+				bind:value={SearchQuery}
+				on:input={searchTable}
+			/>
 		</span>
 	</div>
-	
-	<div>
-		<button type="button" class="hover:variant-filled-error bg-green-500"
-				on:click={() => openModal(1, -1)}>Thêm</button> 
-	</div>
+
 	<div class="table-container">
 		<table class="table table-hover">
 			<thead class="text-center">
 				<tr>
 					{#each fieldNames as fName}
-					<th>{fName}</th>
+						<th>{fName}</th>
 					{/each}
 				</tr>
 			</thead>
@@ -110,7 +126,7 @@
 				{#each studentTable as studentRow, i}
 					<tr>
 						<td>{i + 1}</td>
-						
+
 						<!-- Don't blame me -->
 						<td>{studentRow?.student_id ?? 'Trống'}</td>
 						<td>{studentRow?.full_name ?? 'Trống'}</td>
@@ -122,11 +138,17 @@
 						<td>{studentRow?.class_name ?? 'Trống'}</td>
 
 						<td>
-							<button type="button" class="hover:variant-filled-error bg-green-500"
-							 		on:click={() => openModal(EDIT_STUDENT_MODAL, i)}>Sửa</button>
+							<button
+								type="button"
+								class="hover:variant-filled-error bg-green-500"
+								on:click={() => openModal(EDIT_STUDENT_MODAL, i)}>Sửa</button
+							>
 
-							<button type="button" class="hover:variant-filled-error bg-red-500"
-									on:click={() => openModal(DELETE_STUDENT_MODAL, i)}>Xoá</button>
+							<button
+								type="button"
+								class="hover:variant-filled-error bg-red-500"
+								on:click={() => openModal(DELETE_STUDENT_MODAL, i)}>Xoá</button
+							>
 						</td>
 					</tr>
 				{/each}
