abe86cc0f727979f1b9031ad48fa50bc3f4d135d
TPALT <tphualt@gmail.com>
2023-12-28 18:45:04 +0700

    feature(class): add, edit, delete class
    Just copied from the `student` page :)

--- src/routes/manage/class/+page.svelte
+++ src/routes/manage/class/+page.svelte
@@ -1,23 +1,86 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
+	import { Table, tableMapperValues, type ModalSettings, type TableSource, getModalStore } from '@skeletonlabs/skeleton';
 	import { PlusSolid } from 'svelte-awesome-icons';
+	import modal from './modal.svelte'
 
 	export let data;
 
-	let { supabase, session, classes } = data;
-	$: ({ supabase, session, classes } = data);
+	const modalStore = getModalStore();
 
-	let classTable: any[] = classes as any[];
+	const ADD_CLASS_MODAL = 1,
+		  EDIT_CLASS_MODAL = 2,
+		  DELETE_CLASS_MODAL = 3;
 
-	const tableSimple: TableSource = {
-		head: ['Tên lớp học', 'Giáo viên'],
-		body: tableMapperValues(classTable, ['name', 'full_name']),
-		meta: tableMapperValues(classTable, ['id'])
-	};
+	let { supabase, session, classes, subjects, teachers } = data;
+	$: ({ supabase, session, classes, subjects, teachers } = data);
 
-	function entry(meta: any) {
-		goto(`class/details?id=${meta.detail[0]}`);
+	let classTable: any[];
+	$: classTable = classes as any[];
+	let fieldNames: string[] = ['STT', 'Mã lớp học phần', 'Tên học phần', 'Giáo viên'];
+
+	function entry(index: number) {
+		goto(`class/details?id=` + index);
+	}
+
+	function openModal(type: number, index: number) {
+		let UpcomingMeta: any;
+
+		if (type === ADD_CLASS_MODAL) {
+			UpcomingMeta = {
+				type: ADD_CLASS_MODAL,
+				data: {
+					subjects: subjects,
+					teachers: teachers
+				}
+			};
+		} else if (type === EDIT_CLASS_MODAL) {
+			UpcomingMeta = {
+				type: EDIT_CLASS_MODAL,
+				data: {
+					id: classTable[index].id,
+					current_subject: classTable[index].subject_id,
+					current_teacher: classTable[index].teacher_id,
+					subjects: subjects,
+					teachers: teachers
+				}
+			};
+		} else {
+			UpcomingMeta = {
+				type: DELETE_CLASS_MODAL,
+				data: { id: classTable[index].id }
+			};
+		}
+
+		let DataModal: ModalSettings = {
+			type: 'component',
+			component: { ref: modal },
+			meta: UpcomingMeta
+		};
+
+		modalStore.trigger(DataModal);
+	}
+
+	let SearchQuery: string = '';
+
+	// A pure Javascript function. The dev is dumb. Deal with it lol
+	function searchTable() {
+		let query = SearchQuery.toUpperCase().trim();
+
+		let RowList = document.getElementsByTagName('tr');
+
+		for (let i = 0; i < RowList.length; i++) {
+			let CellList = RowList[i].getElementsByTagName('td');
+
+			for (let j = 0; j < CellList.length; j++) {
+				if (CellList[j] && CellList[j].innerHTML.toUpperCase().indexOf(query) > -1) {
+					RowList[i].style.display = '';
+					break;
+				} else {
+					RowList[i].style.display = 'none';
+				}
+			}
+		}
 	}
 </script>
 
@@ -27,11 +90,52 @@
 			<h2 class="h2">Danh sách lớp học</h2>
 		</span>
 		<span class="flex gap-4">
-			<button class="variant-filled"><PlusSolid size="16" />Thêm</button>
-			<input type="text" name="search-box" id="search-box" placeholder="Tìm kiếm..." disabled />
+			<button class="variant-filled" on:click={() => openModal(ADD_CLASS_MODAL, -1)}><PlusSolid size="16" />Thêm</button>
+			<input 
+				type="text" 
+				name="search-box" 
+				id="search-box" 
+				placeholder="Tìm kiếm..."
+				bind:value={SearchQuery}
+				on:input={searchTable}
+			/>
 		</span>
 	</div>
 	<div class="content">
-		<Table source={tableSimple} interactive={true} on:selected={entry} />
+		<table class="table table-hover">
+			<thead class="text-center">
+				<tr>
+					{#each fieldNames as fName}
+						<th>{fName}</th>
+					{/each}
+				</tr>
+			</thead>
+
+			<tbody>
+				{#each classTable as classRow, i}
+					<tr>
+						<td>{i + 1}</td>
+
+						<!-- Don't blame me -->
+						<td on:click={() => entry(classRow.id)}>{classRow?.id ?? 'Trống'}</td>
+						<td on:click={() => entry(classRow.id)}>{classRow?.name ?? 'Trống'}</td>
+						<td on:click={() => entry(classRow.id)}>{classRow?.full_name ?? 'Trống'}</td>
+						<td>
+							<button
+								type="button"
+								class="hover:variant-filled-error bg-green-500"
+								on:click={() => openModal(EDIT_CLASS_MODAL, i)}>Sửa</button
+							>
+
+							<button
+								type="button"
+								class="hover:variant-filled-error bg-red-500"
+								on:click={() => openModal(DELETE_CLASS_MODAL, i)}>Xoá</button
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
---
+++ src/routes/manage/class/modal.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+	import { ListBox, ListBoxItem, getModalStore, getToastStore } from '@skeletonlabs/skeleton';
+	import type { SvelteComponent } from 'svelte';
+	import type { ActionData } from './$types';
+	import { enhance } from '$app/forms';
+	import { invalidate } from '$app/navigation';
+
+	const modalStore = getModalStore();
+	const toastStore = getToastStore();
+
+	const ADD_CLASS_MODAL: number = 1,
+		EDIT_CLASS_MODAL: number = 2,
+		DELETE_CLASS_MODAL: number = 3;
+
+	const ID_INDEX: number = 10;
+
+	export let parent: SvelteComponent;
+	export let form: ActionData;
+
+	let modalTitle: any;
+	let labels: any = [];
+	let inputNames: any = [];
+	let classData: any = [];
+	let type: any = $modalStore[0].meta.type;
+	let classID: any = $modalStore[0].meta.data.id;
+	let subjectList: any = [];
+	subjectList = $modalStore[0].meta.data.subjects;
+	let teacherList: any = [];
+	teacherList = $modalStore[0].meta.data.teachers;
+	let selectedTeacher: any = $modalStore[0].meta.data.current_teacher;
+	let selectedSubject: any = $modalStore[0].meta.data.current_subject;
+
+	function formEnhance() {
+		return async () => {
+			console.log(form);
+			if (form?.error) {
+				toastStore.trigger({
+					message: 'Không thể xử lí yêu cầu',
+					background: 'variant-filled-error',
+					hideDismiss: true
+				});
+			} else {
+				toastStore.trigger({
+					message: 'Thao tác thành công',
+					background: 'variant-filled-secondary',
+					hideDismiss: true
+				});
+				invalidate('class:reload');
+				modalStore.close();
+			}
+		};
+	}
+
+	labels = ['Mã lớp học phần', 'Tên học phần', 'Giáo viên'];
+	inputNames = ['id', 'subject_name', 'teacher_name'];
+
+	if (type === EDIT_CLASS_MODAL) {
+		modalTitle = 'Chỉnh sửa thông tin';
+		classData = [
+			$modalStore[0].meta.data.id,
+			$modalStore[0].meta.data.name,
+			$modalStore[0].meta.data.full_name
+		];
+	} else if (type === ADD_CLASS_MODAL) {
+		modalTitle = 'Thêm thông tin';
+	} else {
+		modalTitle = 'Xoá thông tin';
+		labels = ['Bạn có chắc chắn về việc xoá lớp này?'];
+	}
+
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	const cHeader = 'text-2xl font-bold';
+	const cForm = 'border border-surface-500 p-4 space-y-4 rounded-container-token';
+</script>
+
+{#if $modalStore[0]}
+	<div class={cBase}>
+		<header class={cHeader}>{modalTitle}</header>
+		<article />
+
+		{#if type === DELETE_CLASS_MODAL}
+			<form class={cForm} method="POST" action="/" use:enhance={formEnhance}>
+				<label for="id" class="label">{labels[0]}</label>
+				<input type="text" name="id" value={classID} hidden />
+				<div class={parent.regionFooter}>
+					<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>Huỷ</button>
+					<button class="btn {parent.buttonPositive}" formaction="?/delete">Xoá</button>
+				</div>
+			</form>
+		{:else}
+			<form class={cForm} method="POST" action="/" use:enhance={formEnhance}>
+				{#each labels as label, i}
+					<label for="df" class="label">
+						<span>{label}</span>
+						{#if inputNames[i] === 'subject_name'}
+							<select name="subject_id" class="select" value={selectedSubject} required>
+								{#each subjectList as subject}
+									<option value={subject.id}>{subject.name}</option>
+								{/each}
+							</select>
+						{:else if inputNames[i] === 'teacher_name'}
+							<select name="teacher_id" class="select" value={selectedTeacher} required>
+								{#each teacherList as teacher}
+									<option value={teacher.id}>{teacher.full_name}</option>
+								{/each}
+							</select>
+						{:else}
+							<input
+								class="input"
+								type="text"
+								name={inputNames[i]}
+								bind:value={classData[i]}
+								placeholder="Không có"
+								required
+							/>
+						{/if}
+					</label>
+				{/each}
+
+				<input name="id" class="input" type="hidden" value={classData[ID_INDEX]} />
+
+				<div class={parent.regionFooter}>
+					<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>Huỷ</button>
+					<button class="btn {parent.buttonPositive}" formaction="?/upsert">Lưu</button>
+				</div>
+			</form>
+		{/if}
+		<!-- prettier-ignore -->
+	</div>
+{/if}
